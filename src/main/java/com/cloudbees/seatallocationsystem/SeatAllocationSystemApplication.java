5d056a0269191fc2d5a47e9a3a2e7d74f5592445
Pavithra <pavithranagarajan205@gmail.com>
2024-03-09 12:35:40 +0530

    Seat allocation system initial commit

---
+++ src/main/java/com/cloudbees/seatallocationsystem/SeatAllocationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.cloudbees.seatallocationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatAllocationSystemApplication {
+
+public static void main(String[] args) {
+
+    SpringApplication.run(SeatAllocationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloudbees/seatallocationsystem/SeatAllocationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudbees.seatallocationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatAllocationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
